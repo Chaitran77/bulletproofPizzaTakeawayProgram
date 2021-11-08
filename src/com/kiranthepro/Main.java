87d7a991a45a5585f8e82ed38b80cc39a547441d
Chaitran77 <kiran@inteleyes.com>
2021-11-08 13:29:13 +0000

    stuff

--- src/com/kiranthepro/Main.java
+++ src/com/kiranthepro/Main.java
@@ -1,15 +1,26 @@
 package com.kiranthepro;
 
-import java.util.ArrayList;
-import java.util.Scanner;
+import java.util.*;
 
 public class Main {
+
+    final static double largePizza = 13.99;
+    final static double mediumPizza = 10.99;
+    final static double smallPizza = 7.99;
+    final static double toppingPrice = 1.25;
 
     public static void main(String[] args) {
 	// write your code here
         Scanner scanner = new Scanner(System.in);
-
+        double pizzaSize = choosePizza(scanner);
         ArrayList<String> pizzaToppings = getToppings(scanner);
+        System.out.println(pizzaToppings.size());
+        System.out.println(pizzaToppings);
+        System.out.println(pizzaSize);
+
+        double price = pizzaSize + (pizzaToppings.size()>2 ? pizzaToppings.size()-2:0 * toppingPrice);
+
+        System.out.println("You have chosen " + String.join(", ", pizzaToppings) + " as your toppings, with a total price of £" + price + ". Please wait while your pizza is prepared...");
     }
 
     public static ArrayList<String> getToppings(Scanner scanner) {
@@ -18,8 +29,9 @@ public class Main {
 
         while (!finished) {
             String newTopping = addTopping(scanner, toppings);
-            if (!newTopping.equals("")) {
+            if (!newTopping.equals("done")) {
                 toppings.add(newTopping);
+                System.out.println("Successfully added " + newTopping + "!");
             } else {
                 finished = true;
             }
@@ -28,34 +40,46 @@ public class Main {
     }
 
     public static String addTopping(Scanner scanner, ArrayList toppings) {
-        System.out.println("Type what topping you want, just hit enter if you're done:");
+        System.out.println("Type what topping you want, just type 'end' if you're done:");
         String input = scanner.next();
 
-        if (checkForExistingTopping(toppings, input`))) {
-            System.out.println("You've already added " + input + ". Please add something else - you can't have too much of a good thing!");
-        }
-        return scanner.next();
-    }
-
-    public static boolean checkForExistingTopping(ArrayList toppings, String toppingInQuestion) {
-        for (Object topping:toppings.toArray()) {
-            if (toppingInQuestion.equalsIgnoreCase(topping.toString())) {
-                return true;
+        if (!input.equals("end")) {
+            if (!checkTopping(toppings, input)) {
+                addTopping(scanner, toppings);
             }
+            return input;
         }
-        return false;
+        return "done";
     }
 
-    public static int choosePizza(Scanner scanner) {
+    public static boolean checkTopping(ArrayList toppings, String toppingInQuestion) {
+        ListIterator toppingsIter = toppings.listIterator();
+
+        if (toppingInQuestion.length() <= 20 || toppingInQuestion.matches(".*\\d.*")) {
+            for (Object topping:toppings.toArray()) {
+                if (toppingInQuestion.equalsIgnoreCase(topping.toString()) || topping.toString().contains(toppingInQuestion)) {
+                    System.out.println("You've already added " + topping + ". Please add something else - you can't have too much of a good thing!");
+                    return false;
+                }
+            }
+        } else {
+            System.out.println("Topping has too long of a name or contains numbers :( Try again: ");
+            return false;
+        }
+
+        return true;
+    }
+
+    public static double choosePizza(Scanner scanner) {
         System.out.println("What size pizza would you like: 'small', 'medium' or 'large'?");
         String pizzaSize = scanner.next();
 
         if (pizzaSize.equalsIgnoreCase("small")) {
-            return 1;
+            return smallPizza;
         } else if (pizzaSize.equalsIgnoreCase("medium")) {
-            return 2;
+            return mediumPizza;
         } else if (pizzaSize.equalsIgnoreCase("large")) {
-            return 3;
+            return largePizza;
         } else {
             System.out.println("Oh no :( Please type either small, medium or large. Try again:");
             choosePizza(scanner);
